e9e2213c19df56366917ad19f804af2abaf52b06
Jens-Christian Fischer <jens-christian.fischer@switch.ch>
2014-09-15 15:55:24 +0200

    0.1.3 releas

---
+++ lib/scale-horizon.py
@@ -0,0 +1,6 @@
+import horizon
+
+settings_dashboard = horizon.get_dashboard("settings")
+password_panel = settings_dashboard.get_panel("password")
+
+settings_dashboard.unregister(password_panel.__class__)
---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup(name='switch',
+      version='0.1.3',
+      package_dir= { 'switch' : 'lib'},
+      py_modules=['switch.scale-horizon'],
+      )
